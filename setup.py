f425ef2bc573dbba7629a66654d0a415705d21c7
kawazome <id.kawazome@gmail.com>
2022-08-20 15:35:33 +0900

    __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='basedir',
-    version="1.0.0",
+    version="1.0.1",
     description="Library that simplifies access to local directories and files",
     long_description="",
     author='kawazome',
